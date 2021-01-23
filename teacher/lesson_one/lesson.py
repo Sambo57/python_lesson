05be9bc0476aef5e462efd7f6e72a7a425d2be28
Sirojiddin <benzema57@mail.ru>
2021-01-23 21:39:30 +0500

    Update some files

--- teacher/lesson_one/lesson.py
+++ teacher/lesson_one/lesson.py
@@ -1,3 +1,8 @@
 # Python comments
 
 print("Hello, world")
+
+# git clone <url>  -> Githubdagi repository ni nusxasini kompyuterga yuklab oladi.
+# git add .  -> o'zgarishlarni karopkalashga tayyorlaydi
+# git commit -m "Komment yozing"  -> Karopkalaydi
+# git push   -> Githubga yuklaydi
