0d6b68a8f00f5c65345b348e5fdca5af5863df22
Sirojiddin <benzema57@mail.ru>
2021-02-06 15:23:38 +0500

    five

--- exercises/execise_four.py
+++ exercises/execise_four.py
@@ -1,125 +1,43 @@
-####################################################
-##############   Strings 
-##########################################
+# x = "awesome"
 
-#######   String are Arrays   #######
+# def myfunc():   
+  #   global x
+  #  x = "fantastic"
 
-# fer = ["H","e", "l", "l", "o"]
-#s = "Hello"
+# myfunc()
 
-# print(fer[4])
-# print(s[1])
+# print("Python is " + x)
 
-#######   String are Arrays   #######
+# a = 5
+# if (5 >= 2):
+  # print("Javob beraman")
+# else:
+  #  print("beramn")
+# x = "apple", "banana" ,"cherry"
+# print(x)
 
-# a = "Olma juda mazzali ekan!"
-
-# print(a[0])
-# print(a[1])
-# print(a[2])
-# print(a[3])
-# print(a[4])
-
-# for it in a:
-#   print(it)
-
-
-#######   String length   #######
-
-#a = "Pineapple"
-#print(len(a))
-
-# posts = ["Title one","Title two", "Title three"]
-
+# posts = ["Title one", "Title two", "Title three", "Title four"]
+# posts = []
 
 # if len(posts) > 0:
-#    print("There are {} posts".format(len(posts)))
-#    for post in posts:
-#        print(post)
-#else:
-#    print("There is no posts yet!")   
-     
-
-#######   Check string   #######
-
-# text = "Bugun havo juda yomon"
-
-# print("juda" in text)
-
-# posts = ["Ob-havo juda yaxshi", "Ob-havo haqida ma'lumotlar", "Xavf yaqinlashmoqda"]
-# query = "Ob-havo"
-
-# for post in posts:
-  #  if query in post:
-   #    print(post)
-
-# txt = "The best things in life are free!"
-# if "free" in txt:
-#  print("Yes, 'free' is present.")
-
-#######   Slicing   #######
-
-# a = "Bugun kompyuterim sekin ishlayapti"
-
-# print(a[5:23])
-# print(a[:35])
-# print(a[-10:-7])
-# print(a[-1])
-
-####################################################
-##############   Modifiying strings 
-####################################################
-
-######### Uppercase #########
-
-# a = "Internet juda sekin "
-# b = "APPLE"
-# c = "     Javohir     "
-# w = "Muslim keldi"
-# l = "Bizda Xumo banan va nok bor"
-
-# print(a.upper())
-# print(b.lower())
-# print(c.strip())
-# print(w.replace("Muslim","Jasur"))
-# print(l.split("  "))
-
-####################################################
-##############   String Concatenation 
-##################################################
-
-#######   String Concatenation   #######
-
-# a = "Assalamu alaykum"
-# b = "Otabek"
-# r = a + " " + b
-# print(r)
-
-####################################################
-##############   String format 
-####################################################
-
-# a = 32
-# t = "mashina"
-# g = "Mening yoshim {} da, lekin men {} xayday olmayman"
-# s = g.format(a,t)
-# print(s)
-
-####################################################
-##############   Escape Characters
-###################################################
-
-# s = 'Bu gapda \"kuchli\" mantiq bor'
-# d = "Bu gapda ma\'no yo'\q" 
-# f = "Bu slash \\ belgisi"
-# g = "Qator 1 \nyangi qator"
-# h = "Qator 1 \rqator boshi"
-# j = "Bu yerda \t tab bor"
+  #  print("There are {} posts".format(len(posts)))
+   # for post in posts:
+    #    print(post)
+# else:
+  #print("Hech nima yo'q")   
+       
 
 
-# print(s)
-# print(d)
-# print(f)
-# print(g)
-# print(h)
-# print(j)
+
+
+
+
+
+
+
+
+
+
+
+import calendar
+print(calendar.month(2020, 12))
---
+++ student/lesson_five/lesson.py
@@ -0,0 +1,154 @@
+####################################################
+##############   Booleans 
+##########################################
+
+
+###### Boolean Values #######
+
+# print(False)
+# print(10 < 0)
+# print(10 > 11)
+# print(10 == 9)
+
+# a = 200
+# b = 33
+
+# if b > a:
+#     print("Iphone")
+# else:
+#     print("Xiaomi")
+
+# print(bool("Hello"))
+# print(bool(1))
+
+# x = "Alhamdulillah"
+# y = 1
+
+# print(bool(x))
+# print(bool(y))
+
+
+#######   Most Values are True   #######
+
+# print(bool("abc"))
+# print(bool(1234))
+# print(bool(["apple", "cherry", "banana"]))
+
+#######   Most Values are True   #######
+
+# print(bool(False))
+# print(bool(None))
+# print(bool(0))
+# print(bool(""))
+# print(bool(()))
+# print(bool([]))
+# print(bool({}))
+
+
+# class myclass():
+#     def_len_(self):
+#         return 0
+
+# myObject = myClass()
+# print(bool(myObject))
+
+
+
+######   Functions can Return a Boolean   #######
+
+# def myFunction():
+#     return True
+
+# print(myFunction())
+
+# if myFunction():
+#     print("YES!")
+# else:
+#     print("NO!")
+
+# x = 200
+# print(isinstance(x, int))
+
+
+####################################################
+##############   Python Operators
+##########################################
+
+
+######   Python Assignment Operators   #######
+
+# 5 ** 3 
+# 5 ** 5
+
+# # print(5 ** 3) 5 * 5 = 25 * 5 = 125
+# print(5 ** 5) 5 * 5 = 25 * 5 = 125 * 5 = 625 * 5 = 3125
+
+# print(7//2)
+
+#######   Python Assignment Operators   #######
+
+# x = 5
+# x = x + 5
+# print(x)
+
+# x = 5
+# x += 5
+# print(x)
+
+#numbers = [0, 3, 23, 45, 543]
+
+# sum = 0
+# sum = sum + numbers[0]
+# sum = sum + numbers[1]
+# sum = sum + numbers[2]
+# sum = sum + numbers[3]
+# sum = sum + numbers[4]
+# print(sum)
+
+# sum = 0
+# sum += numbers[0]
+# sum += numbers[1]
+# sum += numbers[2]
+# sum += numbers[3]
+# sum += numbers[4]
+# print(sum)
+
+# sum = 0
+# for number in numbers:
+#     sum += number
+# print(sum)
+
+# print(True and True)
+# print(True and False)
+# prinnt(False and False)
+
+# print(True & True)
+# print(True & False)
+# prinnt(False & False)
+
+#######   Python Identity Operators   #######
+ 
+# x = 4 
+# y = x
+# z = "Apple"
+
+# print(y is x)
+# print(y is not x)
+# print(z is x)
+
+
+#######   Python Membership Operators   #######
+
+x = "Salom do'stlar"
+y = "Salom"
+
+print(y in x)
+
+a = [3, 10, 6, "Hello", 35.8]
+b = 5
+c = 10
+
+print(b in a)
+print(c in a)
+print(b not in a)
+
