5170125d4208c574d42486db88f3b6f95ae59b7f
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-02-10 19:25:50 +0500

    Add lesson 6

---
+++ teacher/lesson_6/lesson.py
@@ -0,0 +1,41 @@
+# String format() method
+
+# text = "Telefon narxi ${narx:.2f}"
+# print(text.format(narx = 49))
+
+
+####################################################
+##############   List 
+####################################################
+
+
+fruits = ["apple", "banana", "cherry"]
+# print(fruits)
+
+# Ordered
+
+# print(fruits[0])
+# print(fruits[1])
+
+# Changeable
+
+# fruits[0] = "strawberry"
+# print(fruits)
+
+# Allow duplicates
+
+# fruits.append("banana")
+# print(fruits)
+
+# print(len(fruits))
+
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 6, 7, 8.5]
+# list3 = [True, False, True]
+
+# list4 = ["apple", 23, True, "male", False, 5.6]
+
+# print(type(fruits))
+
+newList = list(("apple", "banana", "cherry"))
+print(newList)
