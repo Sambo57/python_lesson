8149a3fc611318187f3ef3aba00634e5cd4e8c97
Sirojiddin <benzema57@mail.ru>
2021-02-12 20:41:05 +0500

    dsd

--- exercises/execise_six.py
+++ exercises/execise_six.py
@@ -45,9 +45,9 @@
 # newlist = [x for x in range(10) if x < 5]
 # print(newlist)
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-newlist = [x.upper() for x in fruits]
-print(newlist)
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = [x.upper() for x in fruits]
+# print(newlist)
 
 # fruits = ["apple", "banana", "cherry","kiwi", "mango"]
 # newlist = ['hello' for x in fruits]
@@ -56,3 +56,168 @@ print(newlist)
 # fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 # newlist = [x if x != "banana" else "orange" for x in fruits]
 # print(newlist)
+
+
+############33 Sort List Alphanumerically ###########33
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = [100, 50, 32,656,232]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort(reverse = True)
+# print(thislist)
+
+# thislist = [100, 50, 65, 22, 434,]
+# thislist.sort(reverse = True)
+# print(thislist)
+
+# def myfunc(n):
+#     return abs(n-50)
+
+# thislist = [100, 50, 65, 76, 17]
+# thislist.sort(key = myfunc)
+# print(thislist)
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = ["banana", "Orange", "Kiwi", "chery"]
+# thislist.sort(key = str.lower)
+# print(thislist)
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.reverse()
+# print(thislist)
+
+
+##########3 Copy List ###########
+
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = list(thislist)
+# print(mylist)
+
+############ Join Lists #############
+
+# list1 = ["a", "b", "c"]
+# list2 = [4,6,8,]
+# list3 = list1 + list2
+# print(list3)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 6]
+
+# for x in list2:
+#     list1.append(x)
+
+# print(list1)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 5]
+# list1.extend(list2)
+# print(list1)
+
+################ List Methods ###############3
+#############################################
+
+
+############ List append ############33
+
+# fruits = [ "apple", "banana", "cherry"]
+# fruits.append("orange")
+# print(fruits)
+
+# a = ["apple", "banana", "cherry"]
+# b = ["Ford", "BMW", "Volvo"]
+# a.append(b)
+# print(a)
+
+############ List clear ################3
+
+# fruits = ["apple", "banana", "cherry", "orange"]
+# fruits.clear()
+# print(fruits)
+
+########## List copy() #############3
+
+# fruits = ["apple", "banana", "cherry", "orange"]
+# x = fruits.copy()
+# print(x)
+
+############# List count ##########
+
+# fruits = ["apple", "banana", "cherry", "cherry"]
+# x = fruits.count("cherry")
+# print(x)
+
+# fruits = [1, 3, 34, 545, 45, 455, 45, 454, 45,]
+# x = fruits.count(45)
+# print(x)
+
+############ List extent() ###########3
+
+# fruits = ["apple", "banana", "cherry"]
+# cars = ["Ford", "BMW", "Mers"]
+# fruits.extend(cars)
+# print(fruits)
+
+# fruits = [ "apple", "banana", "cherry"]
+# points = (1, 4 ,6, 7,4)
+# fruits.extend(points)
+# print(fruits)
+
+#############3 List index() ###########33
+
+# fruits = ["apple", "banana", "cherry"]
+# x = fruits.index("banana")
+# print(x)
+
+# fruits = [4, 56, 343, 355, 34]
+# x = fruits.index(355)
+# print(x)
+
+########### List insert() #############33
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(0, "orange")
+# print(fruits)
+
+
+############3 List pop() #############
+
+# fruits = ["apple", "banana", "pineapple"]
+# fruits.pop(2)
+# print(fruits)
+
+# fruits = ["apple", "banana", "cherry"]
+# x = fruits.pop(2)
+# print(x)
+
+
+############ List remove() #########
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.remove("apple")
+# print(fruits)
+
+
+############## List reverse() #########3333
+
+# fruits = [ "apple", "banana", "mango"]
+# fruits.reverse()
+# print(fruits)
+
+############3 List sort() #############
+cars = ["Ford", "BMW", "Kamaz"]
+cars.sort()
+print(cars)
