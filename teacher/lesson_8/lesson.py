cd635bfbe9a32760a075307b0314e8a9c9f8d85f
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-02-15 21:08:28 +0500

    Add lesson 8

---
+++ teacher/lesson_8/lesson.py
@@ -0,0 +1,25 @@
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple", "banana"}
+
+# z = x.difference(y)
+
+# print(z)
+
+
+# x = {"apple", "banana", "cherry"}
+# y = {"google", "microsoft", "apple", "banana"}
+
+# x.difference_update(y)
+
+# print(x)
+
+
+# x = {"a", "b", "c"}
+# y = {"f", "e", "d", "c", "b", "a"}
+
+# z = x.issubset(y)
+# w = y.issuperset(x)
+
+# print(w)
+# print(z)
